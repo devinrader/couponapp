a3f6328b27ff3e62d374a4a525123d2e3ae2481f
Devin Rader <devin@twilio.com>
2014-10-01 16:26:11 -0400

    Debug all the things

--- app.py
+++ app.py
@@ -4,7 +4,8 @@ from coupons.app import app
 
 
 if __name__ == '__main__':
+    app.debug = True
     port = int(os.environ.get("PORT", 5000))
-    if port == 5000:
-        app.debug = True
+#    if port == 5000:
+#        app.debug = True
     app.run(host='0.0.0.0', port=port)
